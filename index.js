6ddce08fb20134ceca28adcc6ebe13493f8fc392
RMCSa <rafamoca42@gmail.com>
2024-02-22 16:22:02 -0300

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+function print(a) {
+  console.log(a);
+}
+
+const saudacao = () => {
+  print("Olá");
+  var data = new Date();
+  print(
+    data.getHours() < 12
+      ? "Bom dia"
+      : data.getHours() < 18
+      ? "Boa Tarde"
+      : "Boa Noite"
+  );
+};
+
+saudacao();
+
+console.log();
+
